6032f90f6765698390a727aa0fe76f527e0bc902
Luana Santos <santosmeiraluana@gmail.com>
2022-01-01 17:54:36 -0300

    v0.0.4

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
+
+
+
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
-    rg;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    };
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = "11122233309";
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456789;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = "88822233309";
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-cliente3.nome = "Alice";
-cliente3.cpf = "88822233309";
-cliente3.agencia = 1001;
-cliente3.saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
 
-console.log(cliente1, cliente2, cliente3);
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(200);
+
+console.log(contaCorrenteRicardo.saldo);
+console.log(cliente1);
+console.log(cliente2);
