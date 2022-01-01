c60693a296e9300a5757efcf394c5cd43bcd30f2
Luana Santos <santosmeiraluana@gmail.com>
2022-01-01 19:24:00 -0300

    retornando o valor sacado e identando melhor o código

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ class ContaCorrente{
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor;
+            return valor;
         }
     };
 
     depositar(valor){
-        if(valor >0){
-            this._saldo += valor;
-        }
+        if(valor <= 0) 
+        {
+            return;
+        }            
+        this._saldo += valor;
     }
 }
 
@@ -35,11 +38,11 @@ cliente2.cpf = "88822233309";
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(-100);
 contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(100);
 
-contaCorrenteRicardo.sacar(50);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
