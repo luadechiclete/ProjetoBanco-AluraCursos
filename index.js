e745c81c70ba60afc47e8f39a6564b2b5b1db57e
Luana Santos <santosmeiraluana@gmail.com>
2022-01-01 17:19:59 -0300

    v0.0.2

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cliente1Agencia = 1001;
 const cliente1Saldo = 0;
 
 const cliente2Nome = "Alice";
-const cliente1CPF = "11122233309";
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+const cliente2CPF = "88822233309";
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
