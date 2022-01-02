98c216a4be175eaa3fdacab431b95309413528a8
Luana Santos <santosmeiraluana@gmail.com>
2022-01-02 17:33:30 -0300

    transferindo entre contas v0.0.9

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,7 +1,12 @@
+
 export class ContaCorrente{
     agencia;
+    cliente;
+
+
     // #saldo =0 seria uma forma de usar um atributo privado
     _saldo = 0;
+
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -16,5 +21,10 @@ export class ContaCorrente{
             return;
         }            
         this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
