f6cec31116ffc9f09a5bd6575b92983482a730a1
Luana Santos <santosmeiraluana@gmail.com>
2022-01-02 19:42:24 -0300

    atributos estáticos v0.1.0

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
     _cliente;
     // #saldo =0 seria uma forma de usar um atributo privado
@@ -22,9 +23,10 @@ export class ContaCorrente{
         return this._saldo;
     }
 
-    constructor(cliente, agencia){
+    constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
     }
 
 
